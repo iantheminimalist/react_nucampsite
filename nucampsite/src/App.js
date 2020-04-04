215cc4f830a9f15f20818d03be47f076f779b73f
Ian De Jesus <ian.dejesus.fontilea@gmail.com>
2020-04-04 01:28:12 -0700

    into to redux

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -3,17 +3,22 @@ import 'typeface-lobster';
 import 'typeface-open-sans';
 import {BrowserRouter} from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 import Main from './components/MainComponent';
+
+const store = ConfigureStore(); 
 
 class App extends Component {
     render() {
         return (
+          <Provider store={store}> 
           <BrowserRouter>
             <div className='App'>
               <Main />
             </div>
           </BrowserRouter>
-
+          </Provider>
         );
     }
 }
